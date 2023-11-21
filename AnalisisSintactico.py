fa729e1e9c82cb5a67094a577a5b4d56f8e348f7
janmayos <jangasca5@gmail.com>
2023-11-21 12:57:33 -0600

    Deteccion de if sin eval con combinacion de parentesis y compuertas

--- AnalisisSintactico.py
+++ AnalisisSintactico.py
@@ -16,20 +16,57 @@ def determina_bien_escrito(cadena,cadena_lexema,regextokens_linea,numlinea):
 	if(ban):
 		print("Linea no reconocida '"+cadena+"'"+" "+str(numlinea))
 if __name__ == '__main__':
-	regex_base_sintac = {
-		"datonorm" : "(ID|COMILLA_SIMPLE.+COMILLA_SIMPLE|COMILLA_DOBLE.+COMILLA_DOBLE|(Numeros|Numero)(PUNTO(Numeros|Numero))?)",
-		"dato" : "((ID|COMILLA_SIMPLE.+COMILLA_SIMPLE|COMILLA_DOBLE.+COMILLA_DOBLE|(Numeros|Numero)(PUNTO(Numeros|Numero))?)|(PARENTESIS_ABRE(ID|COMILLA_SIMPLE.+COMILLA_SIMPLE|COMILLA_DOBLE.+COMILLA_DOBLE|(Numeros|Numero)(PUNTO(Numeros|Numero))?)PARENTESIS_CIERRA))",
-		"operadores_comparacion" : "(IGUALIGUAL|EXCLAMACIONIGUAL|MENOR_QUE|MAYOR_QUE|MENOR_QUEIGUAL|MAYOR_QUEIGUAL)"
-		
-	}
-	regex_base_complex = {
-		"comparación" : "("+regex_base_sintac["dato"]+")("+regex_base_sintac["operadores_comparacion"]+")("+regex_base_sintac["dato"]+")",
-		"comparacion_parentesis" : "PARENTESIS_ABRE("+"("+regex_base_sintac["dato"]+")("+regex_base_sintac["operadores_comparacion"]+")("+regex_base_sintac["dato"]+")"+")PARENTESIS_CIERRA",
-		"comparacion_full" : "(("+"("+regex_base_sintac["dato"]+")("+regex_base_sintac["operadores_comparacion"]+")("+regex_base_sintac["dato"]+")"+")|("+"PARENTESIS_ABRE("+"("+regex_base_sintac["dato"]+")("+regex_base_sintac["operadores_comparacion"]+")("+regex_base_sintac["dato"]+")"+")PARENTESIS_CIERRA"+"))",			
-	}
+
+	#dato primitivo conocido 
+	datonorm ="((ID)|(COMILLA_SIMPLE.+COMILLA_SIMPLE)|(COMILLA_DOBLE.+COMILLA_DOBLE)|((Numeros|Numero)(PUNTO(Numeros|Numero))?))"
+	
+	#dato con parentesis o sin parentesis
+	dato =  "(("+datonorm+")|(PARENTESIS_ABRE("+datonorm+")PARENTESIS_CIERRA))"
+
+	#operadores de comparacion Menor que: < Mayor que: > Igual: = Diferente: != Menor o igual que: <= Mayor o igual que: >=
+
+	operadores_comparacion = "(IGUALIGUAL|EXCLAMACIONIGUAL|MENOR_QUE|MAYOR_QUE|MENOR_QUEIGUAL|MAYOR_QUEIGUAL)"
+	
+	#Dato con negación
+	dato_exclamacion = "((EXCLAMACION)*"+dato+")"
+
+	#Comparar dos datos primitivos con los operadores e.g. a < 1
+	comparacion="("+dato_exclamacion+operadores_comparacion+dato_exclamacion+")"
+	
+	#Comparar dos datos primitivos con los operadores y encerrado entre parentesis con su posible negacion e.g. 
+	# (a < 1)
+	# !(!a <= 1)
+
+	comparacion_parentesis= "((EXCLAMACION)*PARENTESIS_ABRE("+comparacion+")PARENTESIS_CIERRA)"
+
+	#Combinacion de comparacion con parentesis o sin parentesis
+
+	comparacion_full = "(("+comparacion+")|("+comparacion_parentesis+"))"
+	 
+	# operadores logicos compuerta and or 
+	operadorlogico = "(AMPERSONAMPERSON|PLECAPLECA)"
+	
+	
+	#Union de operadores logicos con comparaciones 
+	comparacion_full_operador_logico="(("+comparacion_full+")(("+operadorlogico+")("+comparacion_full+"))*)"
+	
+	#Union de operadores logicos con comparaciones encerrado entre parentesis 
+	comparacion_full_operador_logico_parentesis = "(PARENTESIS_ABRE("+comparacion_full_operador_logico+")PARENTESIS_CIERRA)"
+	
+	#Union de operadores logicos con comparaciones encerrado entre parentesis o sin
+	comparacion_full_operador_logico_parentesis_full="((("+comparacion_full_operador_logico+")|("+comparacion_full_operador_logico_parentesis+")))"
+	
+	##Union de operadores logicos con comparaciones encerrado entre parentesis o sin, ya sea una o otra
+	comparacion_full_operador_logico_parentesis_full_final = "(("+comparacion_full_operador_logico_parentesis_full+")(("+operadorlogico+")("+comparacion_full_operador_logico_parentesis_full+"))*)+"
+	#print(comparacion_full_operador_logico_parentesis_full_final)
+	#exit(0)
+	
+
+
 	regextokens_linea = {
 		"Variables_asinación" : {
-			"regex": "PR(ID(COMA|(IGUAL(Numeros|COMILLA_SIMPLEIDCOMILLA_SIMPLE|COMILLA_DOBLE(ID)*COMILLA_DOBLE|ID)))*)+PUNTO_COMA"
+			"regex": 
+			"PR(ID(COMA|(IGUAL(Numeros|COMILLA_SIMPLEIDCOMILLA_SIMPLE|COMILLA_DOBLE(ID)*COMILLA_DOBLE|ID)))*)+PUNTO_COMA"
 		},
 		"Definir_funcion" : {
 			"regex" : "(PRID|ID)(PARENTESIS_ABRE((PARENTESIS_CIERRALLAVE_ABRE)|(PARENTESIS_CIERRA)|(PRID(COMAPRID)*((PARENTESIS_CIERRALLAVE_ABRE)|(PARENTESIS_CIERRA)))))"
@@ -48,9 +85,11 @@ if __name__ == '__main__':
 			"Eval" : "usingnamespacestd;"
 		},
 		"if" : {
-			"regex" : "PRPARENTESIS_ABRE"+regex_base_complex["comparacion_full"]+"PARENTESIS_CIERRA",
+			"regex" : "PRPARENTESIS_ABRE("+comparacion_full_operador_logico_parentesis_full_final+")((PARENTESIS_CIERRALLAVE_ABRE)|(PARENTESIS_CIERRA))",
 		}
 	}
+	#print(regextokens_linea["if"]["regex"])
+	#exit(0)
 	#PR_.+(PARENTESIS_ABRE(((PARENTESIS_CIERRALLAVE_ABRE)|(PARENTESIS_CIERRA)|(ID(COMAID)*((PARENTESIS_CIERRALLAVE_ABRE)|(PARENTESIS_CIERRA))))))
 	for listatoken in genera_lista_tokens():
 		cadena_linea = ""
